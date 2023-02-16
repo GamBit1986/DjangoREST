c1729f1ad33c8ecb118e514c991f0fa745982b3f
GamBit1986 <52270313+GamBit1986@users.noreply.github.com>
2023-02-16 11:53:13 +0300

    Make serializers (#1)

--- library/authors/models.py
+++ library/authors/models.py
@@ -5,3 +5,22 @@ class Author(models.Model):
     first_name = models.CharField(max_length=64)
     last_name = models.CharField(max_length=64)
     birthday_year = models.PositiveIntegerField()
+
+    def __str__(self) -> str:
+        return f'{self.first_name} | {self.last_name} | {self.birthday_year}'
+
+
+class Biography(models.Model):
+    text = models.CharField(max_length=1024 ,null=True, blank=True)
+    author = models.OneToOneField(Author, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.author
+
+
+class Book(models.Model):
+    name = models.CharField(max_length=128)
+    authors = models.ManyToManyField(Author)
+
+    def __str__(self) -> str:
+        return self.name
